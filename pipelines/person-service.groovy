b57c587a0116dbfc255fa3a32e2694b1a77123ed
Scott Morgan <smorgan@cardinalsolutions.com>
2019-12-03 15:12:07 -0500

    more work

--- pipelines/person-service.groovy
+++ pipelines/person-service.groovy
@@ -42,8 +42,11 @@ node('maven') {
   }
 
   stage ('Deploy to QA'){
-      input "Deploy version ${newTag} to QA?"
-      
+    input "Deploy version ${newTag} to QA?"
+    sh "oc project ${ocqanamespace}"
+    sh "oc patch dc ${appname} --patch '{\"spec\": { \"triggers\": [ { \"type\": \"ImageChange\", \"imageChangeParams\": { \"containerNames\": [ \"${appname}\" ], \"from\": { \"kind\": \"ImageStreamTag\", \"namespace\": \"${ocdevnamespace}\", \"name\": \"$appname:$newTag\"}}}]}}' -n ${ocqanamespace}"
+	sh "oc rollout latest dc/${appname}"
+    verifyDeployment namespace:ocqanamespace, dc:appname, verbose:true
   }
 }
 
